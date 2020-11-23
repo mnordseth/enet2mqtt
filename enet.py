1b58805790ab6393cea8a492fb8b9a9ca986343a
Magnus Nordseth <magnus@nordseth.net>
2020-11-23 19:06:26 +0000

    Initial

---
+++ enet.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python
+
+import requests
+import hashlib
+import pprint
+import string
+import random
+import json
+import logging
+
+log = logging.getLogger(__name__)
+
+session = requests.Session()
+URL_MANAGEMENT="/jsonrpc/management"
+URL_VIZ="/jsonrpc/visualization"
+URL_COM="/jsonrpc/commissioning"
+
+class EnetClient:
+    def __init__(self, user, passwd, hostname):
+        self.user = user
+        self.passwd = passwd
+        self.hostname = hostname
+        self._session = requests.Session()
+        self._debug_requests = False
+        self._api_counter = 1
+        self._cookie=""
+        self.devices = []
+        #self._cookie="1ahlme9ytcbm311qphlvofmmwz"
+
+    def request(self, url, method, params=None, get_raw=False):
+        
+        req = {"jsonrpc":"2.0",
+               "method":method,
+               "params":params,
+               "id":str(self._api_counter)
+        }
+        self._api_counter += 1
+        response = self._session.post("http://%s%s" % (self.hostname, url), json=req)
+        if get_raw:
+            return response
+        if response.status_code >= 400:
+            log.warning(f"Request to {response.request.url} failed with status {response.status_code}")
+            return response
+        log=dict(request=dict(url=response.request.url,
+                              headers=dict(response.request.headers),
+                              body=req),
+                response=dict(headers=dict(response.headers),
+                              body=response.json()))
+        self._cookie=log["response"]["headers"].get("X-ClientCredentials-SessionId")
+
+        if self._debug_requests:
+            pprint.pprint(log)
+        if False:
+            print(log["request"]["headers"].get("Cookie"))
+            print(log["response"]["headers"].get("X-ClientCredentials-SessionId"))
+        json = response.json()
+        if "error" in json:
+            e = "-> %s %s returned error: %s" %(url, method, json["error"])
+            raise(Exception(e))
+        else:
+            if self._debug_requests:
+                print("-> %s %s returned: %s" % (url, method, json["result"]))
+        return json["result"]
+
+    def _calc_auth_response(self, challenge, cnonce=None):
+        if cnonce is None:
+            cnonce= "".join(random.choice(string.ascii_letters + string.digits) for i in range(40))
+
+        realm = challenge["realm"]
+        nonce = challenge["nonce"]
+        uri = challenge["uri"]
+
+        ha1 = "{}:{}:{}".format(user, realm, passwd)
+        ha1 = hashlib.sha1(ha1.encode()).hexdigest().upper()
+        ha2 = hashlib.sha1(("POST:%s" % uri).encode()).hexdigest().upper()
+        nc="00000001"
+
+        
+        response = "{}:{}:{}:{}:{}:{}".format(ha1, challenge["nonce"], nc, cnonce, "auth", ha2)
+        response = hashlib.sha1(response.encode()).hexdigest().upper()
+
+        params = {"userName":user,
+                   "uri":uri, 
+                   "qop":"auth",
+                   "cnonce":cnonce,
+                   "nc":"00000001",
+                   "response":response,
+                   "realm":realm,
+                   "nonce":nonce,
+                   "algorithm":"sha",
+                   "opaque":challenge["opaque"]
+        }
+        return params
+            
+
+    def simple_login(self):
+        params = dict(userName=self.user,
+                      userPassword=self.passwd)
+        r = self.request(URL_MANAGEMENT, "userLogin", params)
+        r = self.request(URL_MANAGEMENT, "setClientRole", dict(clientRole="CR_VISU"))
+        
+    
+    def login(self):
+        challenge = self.request(URL_MANAGEMENT, "getDigestAuthentificationInfos").json()["result"]
+        response = self._calc_auth_response(challenge)
+        r = self.request(URL_MANAGEMENT, "userLoginDigest", response)
+
+        # For some reason I don't get, this request has to be made for auth to work for following requests...
+        r = self._session.get("http://%s/wslclient.html?icp=6p1C8GeIi2FOOEfeA85a" % self.hostname)
+
+
+    def get_links(self, deviceUIDs=[]):
+        r = self.request(URL_MANAGEMENT, "setClientRole", dict(clientRole="CR_IBN"))
+        
+        #devices = self.request(URL_VIZ, "getDeviceUIDs")
+        #deviceUIDs = [i["deviceUID"] for i in devices["deviceUIDs"]]
+        params={"deviceUIDs":deviceUIDs}
+        result = self.request(URL_COM, "getLinksFromDevices", params)
+        return result
+        
+
+    def request_events(self):
+        return self.request(URL_VIZ, "requestEvents")
+    
+    def get_account(self):
+        return self.request(URL_MANAGEMENT, "getAccount")
+
+    def get_event_id(self, devUID=["83ec3031-f8f0-4972-a92b-2df300000213"]):
+        if type(devUID) is not type([]):
+            devUID=[devUID]
+        params={"deviceUIDs":devUID,
+                  "filter":".+\\\\.(SCV1|SCV2|SNA|PSN)\\\\[(.|1.|2.|3.)\\\\]+"}
+        return self.request(URL_VIZ, "getDevicesWithParameterFilter", params)
+    
+    def get_current_values(self, output_device_uid):
+        {"jsonrpc":"2.0", "method":"getCurrentValuesFromOutputDeviceFunction", "params":{"deviceFunctionUID":"83ec3031-f8f0-4972-a92b-2df300000c70"}, "id":"83"}
+        params = {"deviceFunctionUID":output_device_uid}
+        return self.request(URL_VIZ, "getCurrentValuesFromOutputDeviceFunction", params)
+                                                                                         
+
+    def get_devices_and_links(self):
+        devices = self.request(URL_VIZ, "getDeviceUIDs")
+        deviceUIDs = [i["deviceUID"] for i in devices["deviceUIDs"]]
+        params={"deviceUIDs":deviceUIDs,
+                  "filter":".+\\\\.(SCV1|SCV2|SNA|PSN)\\\\[(.|1.|2.|3.)\\\\]+"}
+        result = self.request(URL_VIZ, "getDevicesWithParameterFilter", params)
+        devices = result["devices"]
+
+        links = self.get_links(deviceUIDs)
+        links_by_devuid = dict([(l["uid"], l) for l in links["devices"]])
+        for device in devices:
+            device.update(links_by_devuid[device["uid"]])
+          
+    def get_devices(self):
+        device_locations = self.get_device_locations()
+        deviceUIDs = list(device_locations.keys())
+        params={"deviceUIDs":deviceUIDs,
+                  "filter":".+\\\\.(SCV1|SCV2|SNA|PSN)\\\\[(.|1.|2.|3.)\\\\]+"}
+        result = self.request(URL_VIZ, "getDevicesWithParameterFilter", params)
+        devices = result["devices"]
+        devices = [Device(self, dev) for dev in devices]
+        for device in devices:
+            device.location = device_locations[device.uid]
+        return devices
+
+    def set_value(self, device_function_uid, value):
+        params = {"deviceFunctionUID":device_function_uid,
+                  "values":[{"valueTypeID":"VT_SCALING_RANGE_0_100_DEF_0",
+                             "value":value}]
+        }
+        result = self.request(URL_VIZ, "callInputDeviceFunction", params)
+        log.debug(f"set_value(): {result}")
+
+    def get_locations(self):
+        params = {"locationUIDs":[]}
+        result = self.request(URL_VIZ, "getLocations", params)        
+        return result["locations"]
+
+    def get_events(self):
+        result = self.request(URL_VIZ, "requestEvents")
+        log.debug("get_events(): " +  result)
+    
+    def foo(self):
+        self.request(URL_VIZ, "getMigratingProjectUID")
+        self.request(URL_COM, "isUpdateProcessActive")
+        self.request(URL_VIZ, "getDeviceCatalogueManufacturerIDs")
+        self.request(URL_VIZ, "getCurrentConfiguration")
+        self.request(URL_VIZ, "getMetaDataFromDeviceCatalogue", params={"manufacturerID":"142"})
+        self.request(URL_VIZ, "getDeviceCatalogueManufacturerIDs")
+
+    def get_device_locations(self):
+        locations = self.get_locations()
+        device_to_loc = {}
+        def recurse_locations(locations, parent=[], level=0):
+            for location in locations:
+                name = location["name"]
+                #print(":".join(parent), name, level)
+                hier_name = ":".join(parent) + ":" + name
+                for device in location["deviceUIDs"]:
+                    #print("  ", device)
+                    device_to_loc[device["deviceUID"]] = hier_name
+                if location["childLocations"]:
+                    parent.append(name)
+                    recurse_locations(location["childLocations"], parent, level+1)
+            if parent:
+                parent.pop()
+        recurse_locations(locations)
+        return device_to_loc
+
+
+
+
+                
+known_actuators = ["DVT_DA1M",  # Jung 1 channel dimming actuator
+                   "DVT_SV1M",  # Jung 1 channel 1-10V dimming actuator
+                   "DVT_DA4R",  # 4 channel dimming actuator rail mount
+                   "DVT_DA1R",  # 1 channel dimming actuator rail mount
+                   "DVT_SJAR"]  # 8 channel switch actuator
+
+known_sensors = ['DVT_WS2BJF50CL', 
+                 'DVT_WS3BJF50', 
+                 'DVT_WS3BJF50CL', 
+                 'DVT_WS4BJF50CL', #
+                 'DVT_BS1BP', # eNet motion detector
+                 'DVT_WS4BJ'] # eNet radio transmitter module 4-gang
+
+def Device(client, raw):
+    device_type = raw["typeID"]
+    if device_type in known_actuators:
+        print("Actuator added: " + raw["typeID"])
+        return Light(client, raw)
+    elif device_type in known_sensors:
+        print("Sensor added: " + raw["typeID"])
+        return Switch(client, raw)
+    else:
+        log.warning(f'Unknown device: typeID={raw["typeID"]} name={raw["installationArea"]}')
+
+
+class BaseEnetDevice:
+    def __init__(self, client, raw):
+        self.client = client
+        self._raw = raw
+        self.channels = []
+        self.uid = self._raw["uid"]
+        self.name = self._raw["installationArea"]
+        self.device_type = self._raw["typeID"]
+        self.battery_state = self._raw["batteryState"]
+        self.software_update_available = self._raw["isSoftwareUpdateAvailable"]
+    def __repr__(self):
+        return "{}(Name: {} Type: {})".format(self.__class__.__name__, self.name, self.device_type)
+
+
+class Switch(BaseEnetDevice):
+    pass
+
+class Light(BaseEnetDevice):
+    def __init__(self, client, raw):
+        super().__init__(client, raw)
+        self.create_channels()
+        
+    def create_channels(self):
+        print(f"Enet Device {self.name} type {self.device_type} has the following channels:")
+        for ccg, channel_config_group in enumerate(self._raw['deviceChannelConfigurationGroups']):
+            for dc, device_channel in enumerate(channel_config_group["deviceChannels"]):
+                print(f"  ccg: {ccg} dc: {dc} Channel type: {device_channel['channelTypeID']} area: {device_channel['effectArea']}")
+                if device_channel["channelTypeID"] != "CT_DEVICE":
+                    c = Channel(self, device_channel)
+                    self.channels.append(c)
+
+                for odf, output_func in enumerate(device_channel["outputDeviceFunctions"]):
+                    type_id = output_func['currentValues'][0]['valueTypeID']
+                    value = output_func['currentValues'][0]['value']
+                    print(f"    odf: {odf} type: {type_id} value: {value}")
+
+    def __repr__(self):
+        return "{}(Name: {} Type: {})".format(self.__class__.__name__, self.name, self.device_type)
+
+
+
+class Channel:
+    def __init__(self, device, raw_channel): #config_group_index = 1, channel_index=0, output_device_function=1):
+        self._device = device
+        self.channel = raw_channel
+        self.uid = f"{self._device.uid}-{self.channel['no']}"
+        self.channel_type = self.channel["channelTypeID"]
+        self._output_device_function = 1
+        self._input_device_function = 2
+        self.name = self.channel["effectArea"]
+        self.has_brightness=False
+        self._iterate_output_functions()
+
+
+    def _iterate_output_functions(self):
+        for odf, output_func in enumerate(self.channel["outputDeviceFunctions"]):
+            type_id = output_func['currentValues'][0]['valueTypeID']
+            value = output_func['currentValues'][0]['value']
+            print(f"    odf: {odf} type: {type_id} value: {value}")
+            if type_id == "VT_SCALING_RANGE_0_100_DEF_0":
+                self.has_brightness = True
+
+    def get_value(self):
+        output_function = self.channel['outputDeviceFunctions'][self._output_device_function]["uid"]
+        current_value = self._device.client.get_current_values(output_function)
+        value = current_value["currentValues"][0]["value"]
+        log.debug(f"{self.name} get_value() returned {value}")
+        self._last_value = value
+        return value
+
+
+    def set_value(self, value):
+        input_function = self.channel['inputDeviceFunctions'][self._input_device_function]["uid"]
+        log.info(f"{self.name} set_value({value})")
+        self._device.client.set_value(input_function, value)
+
+    def turn_off(self):
+        params = {"deviceFunctionUID":self.channel["inputDeviceFunctions"][0]["uid"],
+                  "values":[{"valueTypeID":"VT_SWITCH",
+                             "value":False}]}
+        result = self._device.client.request(URL_VIZ, "callInputDeviceFunction", params)
+        log.info(f"{self.name} turn_off()")
+        return result
+
+    def turn_on(self):
+        params = {"deviceFunctionUID":self.channel["inputDeviceFunctions"][0]["uid"],
+                  "values":[{"valueTypeID":"VT_SWITCH",
+                             "value":True}]}
+        result = self._device.client.request(URL_VIZ, "callInputDeviceFunction", params)
+        log.info(f"{self.name} turn_off()")
+        return result
+
+    def __repr__(self):
+        return "{}(Name: {} Type: {} Value: {})".format(self.__class__.__name__, self.name, self.channel_type, self.get_value())
+        
+
+#import enet2mqtt
+
+# SCENES
+# IBOX
+# {"jsonrpc":"2.0", "method":"getInputDeviceFunctionsFromDevice", "params":{"deviceUID":"83ec3031-f8f0-4972-a92b-2df300000001", "channelNumber":0}, "id":"47"}
+# {"jsonrpc":"2.0","result":{"inputDeviceFunctions":[{"uid":"83ec3031-f8f0-4972-a92b-2df300000005","typeID":"IBOX_INScS.SSGI","active":true,"deviceFunctionDependency":null}]},"id":"47"}
+
+# {"jsonrpc":"2.0", "method":"getOutputDeviceFunctionsFromDevice", "params":{"deviceUID":"83ec3031-f8f0-4972-a92b-2df300000001", "channelNumber":0}, "id":"50"}
+# {"jsonrpc":"2.0","result":{"outputDeviceFunctions":[{"uid":"83ec3031-f8f0-4972-a92b-2df300000008","typeID":"IBOX_INScS.SC","active":true,"currentValues":[{"value":0,"valueTypeID":"VT_SCENE_NUMBER","valueUID":"83ec3031-f8f0-4972-a92b-2df300000006"},{"value":"ACTIVATE","valueTypeID":"VT_IN_SCENE_CONTROL","valueUID":"83ec3031-f8f0-4972-a92b-2df300000007"}],"deviceFunctionDependency":null},{"uid":"83ec3031-f8f0-4972-a92b-2df300000011","typeID":"IBOX_INScS.SSGET","active":true,"currentValues":[{"value":0,"valueTypeID":"VT_SSGET_CONNECTION_CODE","valueUID":"83ec3031-f8f0-4972-a92b-2df300000009"},{"value":0,"valueTypeID":"VT_SSGET_SEQUENCE_NUMBER","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000a"},{"value":0,"valueTypeID":"VT_SSGET_COMMAND","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000b"},{"value":0,"valueTypeID":"VT_SSGET_GROUP_ADDRESS","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000c"},{"value":0,"valueTypeID":"VT_SSGET_SERIAL_NUMBER","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000d"},{"value":"SUMSTATUS_GET","valueTypeID":"VT_SSGET_REQUEST_TYPE","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000e"},{"value":false,"valueTypeID":"VT_SSGET_NO_COMMAND_FLAG","valueUID":"83ec3031-f8f0-4972-a92b-2df30000000f"},{"value":false,"valueTypeID":"VT_SSGET_REPEAT_FLAG","valueUID":"83ec3031-f8f0-4972-a92b-2df300000010"}],"deviceFunctionDependency":null}]},"id":"50"}
+
+params = {"deviceFunctionUID":"83ec3031-f8f0-4972-a92b-2df300000005",
+                  "values":[{"valueTypeID":"VT_SCENE_NUMBER",
+                             "value":0}]}
+
---
+++ enet2mqtt.py
@@ -0,0 +1,166 @@
+import paho.mqtt.client as mqtt
+import enet
+import json
+import time
+import logging
+import argparse
+
+log = logging.getLogger(__name__)
+
+class MqttEnetLight(enet.Channel):
+    def get_ha_mqtt_config(self):
+        location = self._device.location.replace("My home:", "")
+        name=f"{location}:{self.name}"
+
+        config = dict(name=name,
+                      unique_id=self.uid,
+                      brightness=self.has_brightness,
+                      schema="json",
+                      device={"name":f"{location}:{self._device.name}",
+                              "identifiers":[self._device.uid],
+                              "model":self._device.device_type,
+                              "manufacturer":"Jung",
+                          },
+                      platform="mqtt",
+                      brightness_scale=100,
+                      command_topic=f"enet/{self.uid}/set",
+                      state_topic=f"enet/{self.uid}/state")
+        return json.dumps(config)
+
+    def handle_mqtt_set(self, cmd):
+        brightness = cmd.get("brightness")
+        state = cmd.get("state")
+        log.debug(f"cmd=cmd brightness={brightness} state={state}")
+        if state == "ON":
+            if brightness is not None:
+                self.set_value(brightness)
+            else:
+                self.turn_on()
+        elif state == "OFF":
+            self.turn_off()
+            brightness = 0
+        return self.get_mqtt_state(brightness)
+
+    def get_mqtt_state(self, value=None):
+        if value is None:
+            value = self.get_value()
+        state = dict(state="OFF" if value == 0 else "ON",
+                     brightness=value)
+        return json.dumps(state)
+
+enet.Channel = MqttEnetLight
+
+class Enet2MqttBridge(mqtt.Client):
+    def __init__(self, enet_client, mqtt_host, mqtt_port, mqtt_user, mqtt_passwd):
+        super().__init__()
+        self.enet = enet_client
+        self.mqtt_host = mqtt_host
+        self.mqtt_port = mqtt_port
+        self.mqtt_user = mqtt_user
+        self.mqtt_passwd = mqtt_passwd
+        self.device_map = {}
+        self.enet_connect()
+        self.enet_poll_interval = 30
+
+    def enet_connect(self):
+        self.enet.get_account()
+        self.enet.simple_login()
+        self.enet.get_account()
+        enet_devices = self.enet.get_devices()
+
+        # Dump raw devices to file
+        with open("devices.json", "w") as fp:
+            json.dump([d._raw for d in enet_devices], fp, indent=2)
+            print("Dumped all device info to devices.json")
+
+        self.devices = []
+        for device in enet_devices:
+            for channel in device.channels:
+                self.devices.append(channel)
+        self.device_map = {device.uid:device for device in self.devices}
+
+    def on_connect(self, mqttc, obj, flags, rc):
+        log.debug("Connected: rc: {rc}")
+
+    def on_message(self, mqttc, obj, msg):
+        log.info(f"On message: {msg.topic} {msg.payload}")
+
+        prefix, device_uid, command = msg.topic.split("/")
+        if command == "set":
+            device = self.device_map.get(device_uid)
+            if not device:
+                print("Unknown device uid: ", device_uid)
+                return
+            payload = json.loads(msg.payload.decode("utf-8"))
+            state = device.handle_mqtt_set(payload)
+            self.publish(f"enet/{device_uid}/state", state, retain=True)
+        else:
+            log.warning(f"on_message: Unknown command '{command}'")
+
+    def on_publish(self, mqttc, obj, mid):
+        pass
+
+    def on_subscribe(self, mqttc, obj, mid, granted_qos):
+        log.debug(f"Subscribed: {mid} {granted_qos}")
+
+    def on_log(self, mqttc, obj, level, string):
+        pass
+
+    def run(self):
+        self.username_pw_set(self.mqtt_user, self.mqtt_passwd)
+        self.connect(self.mqtt_host, self.mqtt_port, 60)
+        self.subscribe("enet/+/set", 0)
+
+        self.loop_start()
+        self.ha_autodiscover()
+        self.poll_enet()
+        
+    def ha_autodiscover(self):
+        for device in self.device_map.values():
+            config = device.get_ha_mqtt_config()
+            topic = "{}/light/{}/config".format("homeassistant", device.uid)
+            log.debug(f"HA Autoconfigure: {topic} {config}")
+            self.publish(topic, config, retain=True)
+            
+            #self.publish(topic, "")
+
+    def poll_enet(self):
+        published_state = {}
+        while True:
+            log.debug("Polling enet devices...")
+            for device in self.device_map.values():
+                try:
+                    state = device.get_mqtt_state()
+                except:
+                    log.exception("Failed to fetch status from enet:")
+                    time.sleep(self.enet_poll_interval)
+                    continue
+                topic = f"enet/{device.uid}/state"
+                
+                if published_state.get(topic) != state:
+                    self.publish(topic, state, retain=True)
+                    published_state[topic] = state
+            time.sleep(self.enet_poll_interval)
+
+
+def parseargs():
+    parser = argparse.ArgumentParser(description='Publish Enet Smart Home devices to MQTT')
+    parser.add_argument('enet_host', help='Enet server address')
+    parser.add_argument('--enet_user', default="admin")
+    parser.add_argument('--enet_passwd', default="admin")
+    parser.add_argument('mqtt_host', help='MQTT Server address')
+    parser.add_argument('--mqtt_port', default=1883)
+    parser.add_argument('--mqtt_user', default="")
+    parser.add_argument('--mqtt_passwd', default="")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parseargs()
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    
+    enet_client = enet.EnetClient(args.enet_user, args.enet_passwd, args.enet_host)
+    bridge = Enet2MqttBridge(enet_client, args.mqtt_host, args.mqtt_port, args.mqtt_user, args.mqtt_passwd)
+    rc = bridge.run()
